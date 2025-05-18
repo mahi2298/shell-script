3ef3f174445537f3744766a99ae308da996630cf
maheswari <mahi.sgs22@gmail.com>
2025-05-18 19:56:24 +0530

    ifelses

--- ifelse.sh
+++ ifelse.sh
@@ -4,7 +4,21 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR:: Please run the script with root access"
+   exit 1
 else
    echo "You are running with root access"
 fi 
-dnf install mysql -y
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+   echo "MYSQL is not installed... going to install it"
+   dnf install mysql -y
+   if [ $? -eq 0 ]
+   then
+      echo "Installing MYSQL is ... SUCCESS"
+   else
+      echo "Installing MYSQL is ... FAILURE"
+      exit 1
+    fi
+else
+   echo "MySQL is already Installed... NOthing to do"
