ffabdeb2bb389245a789b3b82e918f3506d6fae9
maheswari <mahi.sgs22@gmail.com>
2025-05-19 21:58:01 +0530

    logs

--- loops.sh
+++ loops.sh
@@ -6,12 +6,15 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 mkdir -p $LOGS_FOLDER
 PACKAGES=("mysql" "nginx" "python")
+
+
 if [ $USERID -ne 0 ]
 then 
    echo -e "$R ERROR:: Please run the script with root access $N" | tee -a $LOG_FILE
@@ -21,10 +24,10 @@ else
 
 VALIDATE () {
     if [ $1 -eq 0 ]
-   then 
-      echo -e "Installing $2 is $G SUCCESS ... $N " | tee -a $LOG_FILE
-   else
-      echo -e "Installing $2 is $R FAILURE ... $N " | tee -a $LOG_FILE
+    then 
+       echo -e "Installing $2 is $G SUCCESS ... $N " | tee -a $LOG_FILE
+    else
+       echo -e "Installing $2 is $R FAILURE ... $N " | tee -a $LOG_FILE
 }
 
 for package in ${PACKAGES[@]}
